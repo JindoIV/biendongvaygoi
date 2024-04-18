9188e0d72889fe3dc900afb5b04ea0bfab7c320f
Dao Thien Hieu Loi <thienhieuloice@gmail.com>
2024-04-18 16:07:33 +0700

    add zoom

--- client/src/app/(homepage)/page.tsx
+++ client/src/app/(homepage)/page.tsx
@@ -1,11 +1,28 @@
-'use client'
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import map from "@/assets/Images/map.png";
+import style from "./homepage.module.scss";
+import { useEffect, useState } from "react";
 
 export default function HomePage() {
+  const [abc, setAbc] = useState<number>(0);
+
+  const [isZoom, setIsZoom] = useState<boolean>(false);
+
+  useEffect(() => {
+    let a = 1;
+    console.log(abc);
+  }, [abc]);
+
+  const add = () => {
+    return 2;
+  };
+
   return (
     <>
-      <div>
-        <Image src={"../client/src/assets/Images/map.png"}></Image>
+      <button onClick={() => setIsZoom(!isZoom)}>click me</button>
+      <div className={`${style.img_hover_zoom} ${isZoom ? style.zoom : ""}`}>
+        <img src={"./nhan_vat_chinh.png"} alt={""}></img>
       </div>
     </>
   );
