db0f7ce9bc483618e1071741feb78b12fd2ab06e
KjinLu <khanhnguyennguyen2003@gmail.com>
2024-04-18 15:00:36 +0700

    create homepage

---
+++ client/src/app/(homepage)/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import Image from 'next/image'
+
+export default function HomePage() {
+  return (
+    <>
+      <div>
+        <Image src={"../client/src/assets/Images/map.png"}></Image>
+      </div>
+    </>
+  );
+}
