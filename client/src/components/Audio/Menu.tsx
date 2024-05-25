2256b0b0f4321613a60fc63fba4eea849e026c94
Dao Thien Hieu Loi <thienhieuloice@gmail.com>
2024-05-25 11:25:46 +0700

    change confirm music

--- client/src/components/Audio/Menu.tsx
+++ client/src/components/Audio/Menu.tsx
@@ -3,12 +3,14 @@ import "./Menu.css";
 import Modal from "react-modal";
 import React, { useState, useRef, useEffect } from "react";
 import { Button, Col, Row } from "antd";
+import Loading from "@/components/Loading/Loading";
 
 export default function Menu() {
   const audioElement = useRef<HTMLAudioElement>(null);
   const [isSoundOn, setIsSoundOn] = useState<boolean>(false);
 
   const [modalConfirm, setModalConfirm] = useState<boolean>(true);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     handlePlayMusic();
@@ -40,54 +42,68 @@ export default function Menu() {
     setModalConfirm(false);
   };
 
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <section>
-      <audio ref={audioElement} onEnded={handleAudioEnded}>
-        <source src="./Duong_mot_chieu_a.mp3" type="audio/mpeg"></source>
-      </audio>
+      {isLoading ? (
+        <>
+          <Loading></Loading>
+        </>
+      ) : (
+        <>
+          <audio ref={audioElement} onEnded={handleAudioEnded}>
+            <source src="./Duong_mot_chieu_a.mp3" type="audio/mpeg"></source>
+          </audio>
 
-      <Modal
-        isOpen={modalConfirm}
-        className="ModalConfirmMusic"
-        // onAfterOpen={afterOpenModal}
-        // onRequestClose={onClose}
-        // style={customStyles}
-        contentLabel="Confirm music"
-        overlayClassName="OverlayConfirmMusic"
-      >
-        <div className="ModalContainerConfirmMusic">
-          <div className="BackgroundConfirmMusic">
-            <div className="BoxConfirmMusic">
-              <Row>
-                <h4>Bạn muốn trải nghiệm web với âm thanh không?</h4>
-              </Row>
-              <Row gutter={16}>
-                <Col>
-                  <Button
-                    type="primary"
-                    onClick={() => handleInitSoundOn(true)}
-                  >
-                    Có
-                  </Button>
-                </Col>
-                <Col>
-                  <Button onClick={() => handleInitSoundOn(false)}>
-                    Không
-                  </Button>
-                </Col>
-              </Row>
+          <Modal
+            isOpen={modalConfirm}
+            className="ModalConfirmMusic"
+            // onAfterOpen={afterOpenModal}
+            onRequestClose={() => handleInitSoundOn(false)}
+            // style={customStyles}
+            // contentLabel="Confirm music"
+            overlayClassName="OverlayConfirmMusic"
+          >
+            <div className="ModalContainerConfirmMusic">
+              <div className="BackgroundConfirmMusic">
+                <div className="BoxConfirmMusic">
+                  <Row>
+                    <h4>Bạn muốn trải nghiệm web với âm thanh không?</h4>
+                  </Row>
+                  <Row gutter={16}>
+                    <Col>
+                      <Button
+                        type="primary"
+                        onClick={() => handleInitSoundOn(true)}
+                      >
+                        Có
+                      </Button>
+                    </Col>
+                    <Col>
+                      <Button onClick={() => handleInitSoundOn(false)}>
+                        Không
+                      </Button>
+                    </Col>
+                  </Row>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </Modal>
+          </Modal>
 
-      <div className={"menu"}>
-        <div
-          className={`${isSoundOn ? "am_luong_btn" : "mute_am_luong_btn"}`}
-          onClick={handlePlayAudio}
-        ></div>
-        <div className={"map_btn"}></div>
-      </div>
+          <div className={"menu"}>
+            <div
+              className={`${isSoundOn ? "am_luong_btn" : "mute_am_luong_btn"}`}
+              onClick={handlePlayAudio}
+            ></div>
+            <div className={"map_btn"}></div>
+          </div>
+        </>
+      )}
     </section>
   );
 }
