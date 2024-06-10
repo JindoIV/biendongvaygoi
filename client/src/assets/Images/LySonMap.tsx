5797965fc5e72564ad2106526558a8523c418fcf
KjinLu <khanhnguyennguyen2003@gmail.com>
2024-06-10 21:18:42 +0700

    edit img

--- client/src/assets/Images/LySonMap.tsx
+++ client/src/assets/Images/LySonMap.tsx
@@ -1,35 +1,35 @@
 export { default as cot_co } from "./Co_Thoi_Loi.png";
-export { default as hai_dang } from "./Hai Dang.png";
-export { default as dinh_lang } from "./Dinh Lang.png";
-export { default as trung_bay } from "./Nha Hang Trung Bay.png";
-export { default as nui_lua } from "./Nui Lua.png";
+export { default as hai_dang } from "./HaiDang.png";
+export { default as dinh_lang } from "./DinhLang.png";
+export { default as trung_bay } from "./NhaHangTrungBay.png";
+export { default as nui_lua } from "./NuiLua.png";
 export { default as chua } from "./Chua.png";
-export { default as cang } from "./Cang bien.png";
+export { default as cang } from "./Cangbien.png";
 export { default as star } from "./Star.png";
-export { default as btn_thoat } from "./Nut Thoat.png";
+export { default as btn_thoat } from "./NutThoat.png";
 
 export { default as trungga } from "./trungga.png";
 
 export { default as cangSK } from "./Cang-Ly-Son.jpg";
-export { default as cangSK2 } from "./CANG SA KY.2.png";
+export { default as cangSK2 } from "./CANGSAKY_2.png";
 
-export { default as chuaDuc } from "./CHUA DUC.1.png";
-export { default as chuaDuc2 } from "./CHUA DUC.2.png";
-export { default as chuaHang } from "./CHUA HANG.1.png";
-export { default as chuaHang2 } from "./CHUA HANG.2.png";
-export { default as cotCo1 } from "./COT CO TO QUOC.1.png";
-export { default as cotCo2 } from "./COT CO TO QUOC.2.png";
+export { default as chuaDuc } from "./CHUADUC_1.png";
+export { default as chuaDuc2 } from "./CHUADUC_2.png";
+export { default as chuaHang } from "./CHUAHANG_1.png";
+export { default as chuaHang2 } from "./CHUAHANG_2.png";
+export { default as cotCo1 } from "./COTCOTOQUOC_1.png";
+export { default as cotCo2 } from "./COTCOTOQUOC_2.png";
 
 export { default as anHai1 } from "./dinhlanganhai(1)(1).png";
 export { default as anHai2 } from "./dinh-lang-an-hai-ly-son-2(1)(1).png";
 
-export { default as anVinh1 } from "./DINH LANG AN VINH.1.jpg";
-export { default as anVinh2 } from "./DINH LANG AN VINH.2.jpg";
-export { default as haiDang1 } from "./NGON HAI DANG.1.jpg";
-export { default as haiDang2 } from "./NGON HAI DANG.2.jpg";
-export { default as trungBay1 } from "./NHA TRUNG BAY HAI DOI HOANG SA KIEM QUAN BAC HAI.png";
-export { default as trungBay2 } from "./NHA TRUNG BAY HAI DOI HOANG SA KIEM QUAN BAC HAI.2.png";
-export { default as giengTien1 } from "./NUI GIENG TIEN.1.png";
-export { default as giengTien2 } from "./NUI GIENG TIEN.2.png";
-export { default as thoiLoi1 } from "./NUI THOI LOI.1.png";
-export { default as thoiLoi2 } from "./NUI THOI LOI.2.png";
+export { default as anVinh1 } from "./DINHLANGANVINH_1.jpg";
+export { default as anVinh2 } from "./DINHLANGANVINH_2.jpg";
+export { default as haiDang1 } from "./NGONHAIDANG_1.jpg";
+export { default as haiDang2 } from "./NGONHAIDANG_2.jpg";
+export { default as trungBay1 } from "./NHATRUNGBAYHAIDOIHOANGSAKIEMQUANBACHAI.png";
+export { default as trungBay2 } from "./NHATRUNGBAYHAIDOIHOANGSAKIEMQUANBACHAI_2.png";
+export { default as giengTien1 } from "./NUIGIENGTIEN_1.png";
+export { default as giengTien2 } from "./NUIGIENGTIEN_2.png";
+export { default as thoiLoi1 } from "./NUITHOILOI_1.png";
+export { default as thoiLoi2 } from "./NUITHOILOI_2.png";
