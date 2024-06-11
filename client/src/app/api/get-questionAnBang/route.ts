96edc6595fd33fe3d1404d9f16f67c0433146bf6
Dao Thien Hieu Loi <thienhieuloice@gmail.com>
2024-06-11 17:00:27 +0700

    add new game

--- client/src/app/api/get-questionAnBang/route.ts
+++ client/src/app/api/get-questionAnBang/route.ts
@@ -2,6 +2,6 @@ import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const questions = await sql`SELECT * FROM questionAnBang`;
+  const questions = await sql`SELECT * FROM questionAnBangv1`;
   return NextResponse.json({ questions }, { status: 200 });
 }
--- client/src/app/api/get-questionLySon/route.ts
+++ client/src/app/api/get-questionLySon/route.ts
@@ -2,6 +2,6 @@ import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const questions = await sql`SELECT * FROM questionLySonv2`;
+  const questions = await sql`SELECT * FROM questionLySonv3`;
   return NextResponse.json({ questions }, { status: 200 });
 }
